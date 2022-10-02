4c83f576dade91a26d0d6771d5bcc76fb98548ac
sakamoto keisuke <motikoma5125@gmail.com>
2022-10-02 18:50:14 +0900

    quiz修正

--- jestSample/quiz/starWars.ts
+++ jestSample/quiz/starWars.ts
@@ -4,8 +4,8 @@ import assert from 'assert';
 export const getStarWarsCharacterType = async (starWarsApiService: StarWarsApiService, id: number): Promise<string> => {
     const name = await starWarsApiService.getStarWarsCharacterName(id);
 
-    if(name === "Luke Skywalker") return "JEDI"
-    if(name === "C-3PO") return "ROBOT"
+    if(name === "Luke Skywalker") return "JEDI" // id=1
+    if(name === "C-3PO") return "ROBOT" // id=2
 
     return "UNKNOWN" 
 };
