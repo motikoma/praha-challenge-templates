775bb1cb9580715924764080653c11283123fa29
sakamoto keisuke <motikoma5125@gmail.com>
2022-10-15 22:59:30 +0900

    四則演算 完成

--- jestSample/fourArithmeticOperations/script.ts
+++ jestSample/fourArithmeticOperations/script.ts
@@ -1,3 +1,31 @@
-export const multiply = (a: number, b: number): number => {
-  return a * b;
+export const multiply = (...args: number[]): number | string => {
+  if (args.length === 0) throw Error("引数を1つ以上,30個以下指定してください");
+  if (args.length > 30) throw Error("引数を1つ以上,30個以下指定してください");
+  const result: number = args.reduce((acc, cur): number => acc * cur);
+  if (result > 1000) return "big big number";
+  return result;
+};
+
+export const add = (...args: number[]): number | string => {
+  if (args.length === 0) throw Error("引数を1つ以上,30個以下指定してください");
+  if (args.length > 30) throw Error("引数を1つ以上,30個以下指定してください");
+  const result: number = args.reduce((acc, cur): number => acc + cur);
+  if (result > 1000) return "too big";
+  return result;
+};
+
+export const subtract = (...args: number[]): number | string => {
+  if (args.length === 0) throw Error("引数を1つ以上,30個以下指定してください");
+  if (args.length > 30) throw Error("引数を1つ以上,30個以下指定してください");
+  const result: number = args.reduce((acc, cur): number => acc - cur);
+  if (result < 0) return "negative number";
+  return result;
+};
+
+export const divide = (...args: number[]): number | string => {
+  if (args.length === 0) throw Error("引数を1つ以上,30個以下指定してください");
+  if (args.length > 30) throw Error("引数を1つ以上,30個以下指定してください");
+  const result: number = args.reduce((acc, cur): number => acc / cur);
+  if (!Number.isInteger(result)) return "結果に小数点が含まれています";
+  return result;
 };
